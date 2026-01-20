f44d4780438a2192352c7a69337e767907ac425d
CrimsonOnGit-hub <allaboutwaterdiamond@gmail.com>
2026-01-19 20:38:03 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,52 +2,60 @@ const APP_ID = "512c0979-f7b9-496c-b07b-5d5f753ae9c9";
 const VERSION = "1.0";
 const CHANNEL = "ingame"; 
 
-var chatClient;
-const logBox = document.getElementById("chat-log");
+var client;
+var debugBox = document.getElementById("debug-log");
+var chatBox = document.getElementById("chat-log");
+
+function dbg(msg) { debugBox.innerHTML += "<div>> " + msg + "</div>"; debugBox.scrollTop = debugBox.scrollHeight; }
 
 window.onload = function() {
+    dbg("Checking SDK...");
     if (typeof Photon === 'undefined') {
-        logBox.innerHTML = "<b style='color:red'>ERROR: photon.js failed to load. Check your GitHub file list!</b>";
+        dbg("FATAL: Photon is NOT defined. Check photon.js!");
         return;
     }
 
-    // Initialize Client (Protocol 1 = WSS)
-    chatClient = new Photon.Chat.ChatClient(1, APP_ID, VERSION);
+    try {
+        dbg("Creating ChatClient...");
+        // Protocol 1 = WSS (Secure)
+        client = new Photon.Chat.ChatClient(1, APP_ID, VERSION);
 
-    chatClient.onStateChange = function(state) {
-        if (state === 10) { // Connected
-            logBox.innerHTML += "<div style='color:green'>[CONNECTED] Joined " + CHANNEL + "</div>";
-            chatClient.subscribe([CHANNEL]);
-            document.getElementById("msgInput").disabled = false;
-            document.getElementById("sendBtn").disabled = false;
-        } else {
-            logBox.innerHTML = "Status: State " + state;
-        }
-    };
+        client.onStateChange = function(state) {
+            dbg("State Change: " + state);
+            if (state === 10) { // Connected
+                chatBox.innerHTML = "<b>[CONNECTED]</b> Joined: " + CHANNEL;
+                client.subscribe([CHANNEL]);
+                document.getElementById("msgInput").disabled = false;
+                document.getElementById("sendBtn").disabled = false;
+            }
+        };
 
-    chatClient.onChatMessages = function(channel, senders, messages) {
-        for (var i = 0; i < messages.length; i++) {
-            logBox.innerHTML += "<div><b>" + senders[i] + ":</b> " + messages[i] + "</div>";
-            logBox.scrollTop = logBox.scrollHeight;
-        }
-    };
+        dbg("Setting Heartbeat...");
+        setInterval(function() {
+            if (client) {
+                if (client.Service) client.Service();
+                else if (client.service) client.service();
+            }
+        }, 50);
 
-    // Heartbeat loop
-    setInterval(function() {
-        if (chatClient) {
-            if (chatClient.Service) chatClient.Service();
-            else if (chatClient.service) chatClient.service();
-        }
-    }, 50);
+        dbg("Connecting to Region US...");
+        var name = "WebUser_" + Math.floor(Math.random() * 999);
+        
+        // This is the safest way to connect on GitHub Pages in 2026
+        client.connectToRegionMaster("US", name);
 
-    // Connect to US region
-    chatClient.connectToRegionMaster("US", "WebAdmin_" + Math.floor(Math.random()*1000));
+    } catch (e) {
+        dbg("CRASH: " + e.message);
+    }
 };
 
 window.sendMsg = function() {
     var input = document.getElementById("msgInput");
-    if (input.value && chatClient) {
-        chatClient.publishMessage(CHANNEL, input.value);
+    if (input.value && client) {
+        client.publishMessage(CHANNEL, input.value);
+        chatBox.innerHTML += "<div><b>Me:</b> " + input.value + "</div>";
         input.value = "";
+        chatBox.scrollTop = chatBox.scrollHeight;
     }
 };
+
